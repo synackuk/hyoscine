e0d8bd6936eaeb7e2830c29593a2809f06b32591
synackuk <synackuk@gmail.com>
2020-06-15 18:14:41 +0100

    Add dirhelper for iOS 6

--- hyoscine/launchd/modules/jb_init.c
+++ hyoscine/launchd/modules/jb_init.c
@@ -9,9 +9,13 @@ int install_jb_init() {
 	if(ret != 0) {
 		return -1;
 	}
-	ret = install("/files/com.n1ghtshade.rtbuddyd.plist", "/mnt/System/Library/LaunchDaemons/com.n1ghtshade.rtbuddyd.plist", 0, 80, 0644);
+
+	unlink("/mnt/usr/libexec/dirhelper");
+	
+	ret = install("/modules/jb_init", "/mnt/usr/libexec/dirhelper", 0, 0, 0755);
 	if(ret != 0) {
 		return -1;
 	}
+
 	return 0;
 }
--- physostigmine/jb_init/main.m
+++ physostigmine/jb_init/main.m
@@ -4,11 +4,37 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+// https://stackoverflow.com/a/2336245
+
+static void mkdir_recursive(const char *dir, int mode) {
+	char tmp[256];
+	char *p = NULL;
+	size_t len;
+
+	snprintf(tmp, sizeof(tmp),"%s",dir);
+	len = strlen(tmp);
+	if(tmp[len - 1] == '/'){
+		tmp[len - 1] = 0;
+	}
+	for(p = tmp + 1; *p; p++){
+		if(*p == '/') {
+			*p = 0;
+			mkdir(tmp, mode);
+			*p = '/';
+		}
+	}
+	mkdir(tmp, mode);
+}
+
 int main(int argc, char *argv[], char *envp[]) {
 	int ret;
 	int console = open("/dev/console", O_WRONLY);
 	dup2(console, 1);
 	dup2(console, 2);
+
+	if(argc > 1 && !strcmp(argv[1], "-machineBoot")) {
+		mkdir_recursive("/var/tmp/launchd", 700);
+	}
 
 	printf("n1ghtshade jb loader by synackuk\n");
 
